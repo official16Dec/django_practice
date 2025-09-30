662299f7ebeeb7a200cc98cd771d893ba0306dbd
official16Dec <officialsunil16@gmail.com>
2025-09-30 21:46:08 +0530

    Handling and mapping templates to the views to the urls.

--- first_project/helloworld/urls.py
+++ first_project/helloworld/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('hello/', views.say_hello),
     path('welcome/', views.welcome),
     path('welcome/<str:username>/', views.welcome_user),
+    path('hello_user/', views.hello_user),
 ]
--- first_project/helloworld/views.py
+++ first_project/helloworld/views.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 def say_hello(request) :
     return HttpResponse("Hello, World!")
 
-
 def welcome(request) :
     return HttpResponse("Welcome to Django!")
 
 def welcome_user(request, username) :
-    return HttpResponse(f"Welcome, {username}!")
+    return HttpResponse("Welcome, {username}!")
+
+def hello_user(request) :
+    return render(request, 'hello.html')
